d211185fad9f748d8364718b28e5239ae0b8a71a
Nbiba Bedis <bedisnbiba@gmail.com>
2023-01-21 18:41:28 +0100

    fmt

--- src/main.js
+++ src/main.js
@@ -1,24 +1,10 @@
 import Chart from "chart.js/auto";
-import 'chartjs-adapter-date-fns';
-import { de } from 'date-fns/locale';
-
 import ChartStreaming from 'chartjs-plugin-streaming';
-
-
-
+import 'chartjs-adapter-date-fns';
 
 
 const main = async function() {
   Chart.register(ChartStreaming);
-
-  // const socket = new WebSocket('ws://localhost:8000');
-  // console.log("opened socket")
-  // socket.onmessage = (e) => {
-  //   console.log(e)
-  // }
-
-
-  // your event listener code - assuming the event object has the timestamp and value properties
 
   const myChart = new Chart(
     document.getElementById("main"),
@@ -50,6 +36,5 @@ const main = async function() {
     myChart.update('quiet');
   }
 }
-
 
 main()
